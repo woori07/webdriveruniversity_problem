51450ba10ad14db163bed1ce3602e75b21d1a252
woori07 <gwgwl1017@gmail.com>
2024-01-14 17:05:38 +0900

    P1

--- login_portal.py
+++ login_portal.py
@@ -1,7 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
