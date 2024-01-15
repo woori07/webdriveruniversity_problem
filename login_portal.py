ad82f5c981ca3a575fcf453fdca2c157ac0d23e7
woori07 <gwgwl1017@gmail.com>
2024-01-15 21:29:33 +0900

    P1

--- login_portal.py
+++ login_portal.py
@@ -4,25 +4,28 @@ import time
 
 class LoginPortal_Problem():
     def xpath_based_click(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome()        # xpath 기반 클릭 기법
         self.driver.get("https://webdriveruniversity.com/Click-Buttons/index.html")
         self.driver.find_element(By.XPATH, '//*[@id="button1"]').click()
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//*[@id="myModalClick"]/div/div/div[3]/button').click()
         
-    def css_based_click(self):
+    def css_based_click(self):      # css 기반 클릭 기법
         self.driver.find_element(By.CSS_SELECTOR, '#button2').click()
         time.sleep(1)
         self.driver.find_element(By.CSS_SELECTOR, '#myModalJSClick > div > div > div.modal-footer > button').click()
         
-    def anylocator_click(self):
+    def anylocator_click(self):     # ID 기반 클릭 기법
         self.driver.find_element(By.ID, 'button3').click()
         time.sleep(1)
         self.driver.find_element(By.XPATH, '//*[@id="myModalMoveClick"]/div/div/div[3]/button').click()
         time.sleep(1)
 
 if __name__ == "__main__":
+    # 3가지의 클릭 기법 사용 후 팝업 닫기
     clicktest = LoginPortal_Problem()
     clicktest.xpath_based_click()
     clicktest.css_based_click()
     clicktest.anylocator_click()
+    
+    
